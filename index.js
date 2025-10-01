4951d56b2e6d9f8cf1c87588d73d5ed5223e400e
Letícia Graziele <leticiagrazielle258@gmail.com>
2025-10-01 18:30:36 +0000

    concluida

---
+++ index.js
@@ -0,0 +1,11 @@
+const { query } = require("./infra/database");
+
+console.log(process.env.NAME);
+
+// query("SELECT 1 + 1;")
+//   .then(function (value) {
+//     console.log(value);
+//   })
+//   .catch(function (err) {
+//     console.log("Deu ruim", err);
+//   });
--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,17 @@
 const {Cliente, Client} = require("pg");
 
 
-async function query(aql){
+async function query(sql, args=[]){
     const client = new Client({
-        host: "",
-        port: "",
-        user: "",
-        password: "",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        database: process.env.DB.NAME,
+        password: process.env.DB_PASS,
     });
     await client.connect();
 
-    const res = await client.query(aql, args);
+    const res = await client.query(sql, args);
 
     await client.end();
     return res.rows;
